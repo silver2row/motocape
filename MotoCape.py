da29abd548dcfac7f9c7c911533ffeddec57e835
silver2row <silver1row@cox.net>
2018-10-12 21:12:13 -0500

    change is good!

--- MotoCape.py
+++ MotoCape.py
@@ -6,68 +6,65 @@ import Adafruit_BBIO.PWM as PWM
 import time
 
 motorOne = "P8_18"
-#motorTwo = "P8_16"
 motorThree = "P8_14"
-#motorFour = "P8_26"
 
 GPIO.setup("P8_18", GPIO.OUT)
-#GPIO.setup("P8_16", GPIO.OUT)
 GPIO.setup("P8_14", GPIO.OUT)
-#GPIO.setup("P8_26", GPIO.OUT)
+
 
 GPIO.output("P8_18", GPIO.LOW)
-#GPIO.output("P8_16", GPIO.LOW)
 GPIO.output("P8_14", GPIO.LOW)
-#GPIO.output("P8_26", GPIO.LOW)
+
 
 dirOne = "P9_16"
-#dirTwo = "P9_14"
 dirThree = "P8_13"
-#dirFour = "P8_19"
 
 app = Flask(__name__)
 @app.route("/")
 @app.route("/<state>")
 
 def updates(state=None):
+
     if state == "F":
-        print "Robot Moving Forward"
+        print "Geaux Forward"
         PWM.start(dirOne, 100)
         PWM.start(dirThree, 100)
         GPIO.output("P8_18", GPIO.HIGH)
-        #GPIO.output("P8_16", GPIO.LOW)
         GPIO.output("P8_14", GPIO.HIGH)
-        #GPIO.output("P8_26", GPIO.LOW)
-        #PWM.start(dirOne, 75)
-        #PWM.start(dirThree, 75)
         time.sleep(.2)
+
     if state == "R":
-        print "Robot Turning Right"
+        print "Geaux Right"
         PWM.start(dirOne, 100)
+        PWM.start(dirThree, 0)
         GPIO.output("P8_18", GPIO.HIGH)
-        #GPIO.output("P8_16", GPIO.LOW)
         GPIO.output("P8_14", GPIO.LOW)
-        #GPIO.output("P8_26", GPIO.LOW)
-        #PWM.start(dirOne, 75)
         time.sleep(.2)
+
     if state == "L":
-        print "Robot Turning Left"
-        PWM.start(dirThree, 100)
+        print "Geaux Left"
+        PWM.start(dirOne, 0)
+        PWM.start(dirThree, 75) 
         GPIO.output("P8_18", GPIO.LOW)
-        #GPIO.output("P8_16", GPIO.LOW)
         GPIO.output("P8_14", GPIO.HIGH)
-        #GPIO.output("P8_26", GPIO.LOW)
-        #PWM.start(ledThree, 0)
         time.sleep(.2)
+
     if state == "S":
-        print "Robot Stopped"
+        print "Stop!"
         PWM.start(dirOne, 0)
         PWM.start(dirThree, 0)
         GPIO.output("P8_18", GPIO.LOW)
-        #GPIO.output("P8_16", GPIO.LOW)
         GPIO.output("P8_14", GPIO.LOW)
-        #GPIO.output("P8_26", GPIO.LOW)
         time.sleep(.2)
+
+    if state == "REV":
+        print "Reverse!"
+        PWM.start(dirOne, -75)
+        PWM.start(dirOne, -75)
+        GPIO.output("P8_18", GPIO.HIGH)
+        GPIO.output("P8_14", GPIO.HIGH)
+        time.sleep(.2)
+
     template_data = {
         "title" : state,
     }

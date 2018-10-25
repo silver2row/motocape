4c3be2b692be888a1fdd3fc18066eeb68806ce7d
silver2row <silver1row@cox.net>
2018-10-24 22:59:16 -0500

    making moves

--- MotoCape.py
+++ MotoCape.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
-import time
 
 motorOne = "P8_18"
 motorFour = "P8_26"
@@ -18,7 +17,6 @@ GPIO.output("P8_26", GPIO.LOW)
 
 dirOne = "P9_16"
 dirFour = "P8_19"
-
 app = Flask(__name__)
 @app.route("/")
 @app.route("/<state>")
@@ -26,45 +24,27 @@ app = Flask(__name__)
 def updates(state=None):
 
     if state == "F":
-        PWM.start(dirOne, 100, 2000, 0)
-        PWM.start(dirFour, 100, 2000, 0)
+        PWM.start(dirOne, 100)
+        PWM.start(dirFour, 100)
         GPIO.output("P8_18", GPIO.HIGH)
         GPIO.output("P8_26", GPIO.HIGH)
 
-    if state == "R":
-        PWM.start(dirOne, 75, 2000, 0)
-        PWM.start(dirFour, 0, 2000, 0)
+    if state == "L":
+        PWM.start(dirOne, 75)
+        PWM.start(dirFour, 0)
         GPIO.output("P8_18", GPIO.HIGH)
         GPIO.output("P8_26", GPIO.LOW)
 
-    if state == "L":
-        PWM.start(dirOne, 0, 2000, 0)
-        PWM.start(dirFour, 75, 2000, 0)
+    if state == "R":
+        PWM.start(dirOne, 0)
+        PWM.start(dirFour, 75)
         GPIO.output("P8_18", GPIO.LOW)
         GPIO.output("P8_26", GPIO.HIGH)
 
     if state == "S":
-        PWM.start(dirOne, 0, 2000, 0)
-        PWM.start(dirFour, 0, 2000, 0)
+        PWM.start(dirOne, 0)
+        PWM.start(dirFour, 0)
         GPIO.output("P8_18", GPIO.LOW)
-        GPIO.output("P8_26", GPIO.LOW)
-
-    if state == "REV":
-        PWM.start(dirOne, 80, 2000, 1)
-        PWM.start(dirFour, 80, 2000, 1)
-        GPIO.output("P8_18", GPIO.HIGH)
-        GPIO.output("P8_26", GPIO.HIGH)
-
-    if state == "REV_L":
-        PWM.start(dirOne, 0, 2000, 1)
-        PWM.start(dirFour, 75, 2000, 1)
-        GPIO.output("P8_18", GPIO.LOW)
-        GPIO.output("P8_26", GPIO.HIGH)
-
-    if state == "REV_R":
-        PWM.start(dirOne, 75, 2000, 1)
-        PWM.start(dirFour, 0, 2000, 1)
-        GPIO.output("P8_18", GPIO.HIGH)
         GPIO.output("P8_26", GPIO.LOW)
 
     template_data = {

301629b6ab29d806f68df1cdc08cd3a38017285e
silver2row <silver1row@cox.net>
2018-10-24 21:21:46 -0500

    making five point turns...

--- MotoCape.py
+++ MotoCape.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from flask import Flask, render_template
 import Adafruit_BBIO.GPIO as GPIO
@@ -6,18 +6,18 @@ import Adafruit_BBIO.PWM as PWM
 import time
 
 motorOne = "P8_18"
-motorThree = "P8_14"
+motorFour = "P8_26"
 
 GPIO.setup("P8_18", GPIO.OUT)
-GPIO.setup("P8_14", GPIO.OUT)
+GPIO.setup("P8_26", GPIO.OUT)
 
 
 GPIO.output("P8_18", GPIO.LOW)
-GPIO.output("P8_14", GPIO.LOW)
+GPIO.output("P8_26", GPIO.LOW)
 
 
 dirOne = "P9_16"
-dirThree = "P8_13"
+dirFour = "P8_13"
 
 app = Flask(__name__)
 @app.route("/")
@@ -26,34 +26,46 @@ app = Flask(__name__)
 def updates(state=None):
 
     if state == "F":
-        PWM.start(dirOne, 100)
-        PWM.start(dirThree, 100)
+        PWM.start(dirOne, 100, 2000, 0)
+        PWM.start(dirFour, 100, 2000, 0)
         GPIO.output("P8_18", GPIO.HIGH)
-        GPIO.output("P8_14", GPIO.HIGH)
+        GPIO.output("P8_26", GPIO.HIGH)
 
     if state == "R":
-        PWM.start(dirOne, 75)
-        PWM.start(dirThree, 0)
+        PWM.start(dirOne, 75, 2000, 0)
+        PWM.start(dirFour, 0, 2000, 0)
         GPIO.output("P8_18", GPIO.HIGH)
-        GPIO.output("P8_14", GPIO.LOW)
+        GPIO.output("P8_26", GPIO.LOW)
 
     if state == "L":
-        PWM.start(dirOne, 0)
-        PWM.start(dirThree, 75)
+        PWM.start(dirOne, 0, 2000, 0)
+        PWM.start(dirFour, 75, 2000, 0)
         GPIO.output("P8_18", GPIO.LOW)
-        GPIO.output("P8_14", GPIO.HIGH)
+        GPIO.output("P8_26", GPIO.HIGH)
 
     if state == "S":
-        PWM.start(dirOne, 0)
-        PWM.start(dirThree, 0)
+        PWM.start(dirOne, 0, 2000, 0)
+        PWM.start(dirFour, 0, 2000, 0)
         GPIO.output("P8_18", GPIO.LOW)
-        GPIO.output("P8_14", GPIO.LOW)
+        GPIO.output("P8_26", GPIO.LOW)
 
-    #if state == "REV":
-        #PWM.start(dirOne, 80, 1)
-        #PWM.start(dirThree, 80, 1)
-        #GPIO.output("P8_18", GPIO.HIGH)
-        #GPIO.output("P8_14", GPIO.HIGH)
+    if state == "REV":
+        PWM.start(dirOne, 80, 2000, 1)
+        PWM.start(dirFour, 80, 2000, 1)
+        GPIO.output("P8_18", GPIO.HIGH)
+        GPIO.output("P8_26", GPIO.HIGH)
+
+    if state == "REV_R":
+        PWM.start(dirOne, 75, 2000, 1)
+        PWM.start(dirFour, 0, 2000, 1)
+        GPIO.output("P8_18", GPIO.HIGH)
+        GPIO.output("P8_26", GPIO.LOW)
+
+    if state == "REV_L":
+        PWM.start(dirOne, 0, 2000, 1)
+        PWM.start(dirFour, 75, 2000, 1)
+        GPIO.output("P8_18", GPIO.LOW)
+        GPIO.output("P8_26", GPIO.HIGH)
 
     template_data = {
         "title" : state,
